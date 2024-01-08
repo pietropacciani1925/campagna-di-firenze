463926abb5864f0578ca34c27953858ac6d4cd7b
pietropacciani1925 <155971390+pietropacciani1925@users.noreply.github.com>
2024-01-08 16:47:30 +0100

    Add files via upload

--- ex04.c
+++ ex04.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    float number1, number2, somma;
+    float number1, number2, somma, divisione, moltiplicazione, differenza;
 printf("inserisci il primo numero: ");
 scanf("%f", &number1);
 printf("inserisci il secondo numero: ");
 scanf("%f", &number2);
 somma = number1 + number2;
 printf("la somma di %f e %f è %.2f\n", number1, number2, somma);
-return 0;
+divisione = number1 / number2;
+printf("la divisione di %f e %f è %.2f\n", number1, number2, divisione);
+moltiplicazione = number1 * number2;
+printf("la moltiplicazione di %f e %f è %.2f\n", number1, number2, moltiplicazione);
+differenza = number1 - number2;
+printf("la differenza di %f e %f è %.2f\n", number1, number2, differenza);
+return 0; 
 
 
 }
