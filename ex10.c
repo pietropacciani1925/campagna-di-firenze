29007cf8ead2509f8ec86deb0d5327d0b6a92e3c
pietropacciani1925 <155971390+pietropacciani1925@users.noreply.github.com>
2024-01-09 15:19:09 +0100

    Add files via upload

---
+++ ex10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("inserisci un numero:");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++){
+       printf(" imbesi muori ");
+    }
+
+}
