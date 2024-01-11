d630231ed945bb16064130ef069d4fa9fedf84c3
pietropacciani1925 <155971390+pietropacciani1925@users.noreply.github.com>
2024-01-11 17:00:54 +0100

    Add files via upload

---
+++ exparking.c
@@ -0,0 +1,39 @@
+#include <stdio.h> 
+int main() {
+   int numerozona;
+   char *via1[] = {" Via Massa 4"};
+   char *via2[] = {" Via della Condotta 7"};
+   char *via3[] = {" Via dei Pescioni 23"};
+   char *via5[] = {"Via del Baluardo 25"};
+   char *via6[] = {"Via della Camorra 2"};
+   char *via7[] = {" Via San Carlo 69"};
+   char *via8[] = {" Via Lorenzo Maitani 99"};
+   char *via9[] = {" Via Valdarno 13"};
+   char *via10[] = {"Viale degli Astronauti 41"};
+printf("\n\nCiao benvenuto in FlashParking!!!\n Grazie al GPS rileviamo che sei a Firenze \n Per trovare un parcheggio scrivi il numero della zona in cui lo desideri:\n 1 Centro Storico\n 2 Campo di Marte \n 3 Gavinana-Galluzzo \n 4 Isolotto-Legnaia \n 5 Rifredi \n INSERISCI IL NUMERO QUI:\n");
+scanf("%d",&numerozona);
+printf("La zona inserita è: %d\n", numerozona);
+    if (numerozona < 1 || numerozona > 5) {
+        printf("La zona inserita non esiste\n");
+ } else {
+        printf("I parcheggi che abbiamo trovato sono %d sono:\n", numerozona);
+        
+        if (numerozona == 1) {
+            printf("1. %s\n", via1[0]);
+            printf("2. %s\n", via2[0]);
+        } else if (numerozona == 2) {
+            printf("1. %s\n", via3[0]);
+            printf("2. %s\n", via4[0]);
+        } else if (numerozona == 3) {
+            printf("1. %s\n", via5[0]);
+            printf("2. %s\n", via6[0]);
+        } else if (numerozona == 4) {
+            printf("1. %s\n", via7[0]);
+            printf("2. %s\n", via8[0]);
+        } else if (numerozona == 5) {
+            printf("1. %s\n", via9[0]);
+            printf("2. %s\n", via10[0]);
+        }
+    }
+}
+    
